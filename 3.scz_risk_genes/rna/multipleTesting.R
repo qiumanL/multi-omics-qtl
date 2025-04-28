6751c7ddcf44d153c161561a247b38730ff70422
qiumanL <157418504+qiumanL@users.noreply.github.com>
2025-04-27 23:04:58 -0400

    Delete 3.scz_risk_genes directory

--- 3.scz_risk_genes/rna/multipleTesting.R
+++
@@ -1,9 +0,0 @@
-Args <- commandArgs(TRUE)
-f<-Args[1]
-d<-read.csv(f,head=T)
-library("qvalue")
-d$qvalue<-qvalue(d$pvalue)$qvalues
-d$FDR<-p.adjust(d$pvalue, method="fdr")
-d$bonferroni<-p.adjust(d$pvalue, method="bonferroni")
-write.table(d,file=paste(f,".multiTest",sep=""),sep="\t",row.names=F,quote=F,col.names=T)
-
